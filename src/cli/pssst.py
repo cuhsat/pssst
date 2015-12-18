d0fb543e111405aa6bdd023af8130b26dd9e542f
Christian Uhsat <christian@uhsat.de>
2015-12-18 17:01:08 +0100

    Update pssst.py

--- src/cli/pssst.py
+++ src/cli/pssst.py
@@ -45,7 +45,7 @@ except ImportError:
     sys.exit("Requires PyCrypto (https://github.com/dlitz/pycrypto)")
 
 
-__all__, __version__ = ["Pssst"], "2.4.4"
+__all__, __version__ = ["Pssst"], "2.4.5"
 
 
 def _encode64(data): # Utility shortcut
